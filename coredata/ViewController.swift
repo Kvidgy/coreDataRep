c724ba1778c8299fd21b830a2f1e21d1cf04a6c5
Evgenii Rebov <evgenii96rebov@gmail.com>
2020-10-09 21:38:06 +0300

    to rep

--- coredata/ViewController.swift
+++ coredata/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello today now")
-        print("hello")
+        print("hello word")
 
     }
 }
